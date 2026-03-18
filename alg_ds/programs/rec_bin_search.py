7bae0847a3404128b0d789d2203316e8198a266a
michaelbowser <michael@michaelbowser.org>
2026-03-18 11:58:16 -0500

    added completed rec_bin_search.py

--- alg_ds/programs/rec_bin_search.py
+++ alg_ds/programs/rec_bin_search.py
@@ -1,31 +1,23 @@
-def binary_search(list, target):
-    first = 0
-    last = len(list) -1
-
-    while first <= last:
-        midpoint = (first + last)//2
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list))//2
 
         if list[midpoint] == target:
-            return midpoint
-        elif list[midpoint] < target:
-            first = midpoint + 1
+            return True
         else:
-            last = midpoint -1
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint + 1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
 
-    return None
+def verify(result):
+    print("Target found: ", result)
 
-def verify(index):
-    if index is not None:
-        print("Target found at index: ", index)
-    else:
-        print("Target not found in list")
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
-
-result = binary_search(numbers, 10)
+numbers = [1,2,3,4,5,6,7,8]
+result = recursive_binary_search(numbers, 12)
 verify(result)
 
-result = binary_search(numbers, 6)
-
+result = result = recursive_binary_search(numbers, 6)
 verify(result)
-# Testing save as
