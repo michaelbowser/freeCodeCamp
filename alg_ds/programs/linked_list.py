b345dd4a044466c2428141b8df42b97018718692
michaelbowser <michael@michaelbowser.org>
2026-03-21 16:29:17 -0500

    added to linked_list.py"

--- alg_ds/programs/linked_list.py
+++ alg_ds/programs/linked_list.py
@@ -20,3 +20,20 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+        
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Takes O(n) time 
+        """
+        current = self.head 
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node 
+
+        return count 
